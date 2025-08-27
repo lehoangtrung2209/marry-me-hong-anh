ffbcab03f6fd0acb7810dd8fe40a0bfb61ace82c
lehoangtrung2209 <33251852+lehoangtrung2209@users.noreply.github.com>
2025-08-27 16:44:16 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+// Nếu dùng React thì thêm plugin react
+// import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/marry-me-hong-anh/', // 👈 quan trọng: đúng tên repo
+  // plugins: [react()],
+})
